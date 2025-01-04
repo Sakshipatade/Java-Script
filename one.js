e3af2fbdc8fce067a6d351f784e2bde7b8ed6c2a
Sakshipatade <sakshivpatade@gmail.com>
2025-01-04 22:09:04 +0530

    my first commmit

---
+++ one.js
@@ -0,0 +1,10 @@
+const accountId=67622
+let accountEmail="sakshi@gmail.com"
+let accountCity="pune"
+let accountPassword="76548"
+let accountStatus;
+
+console.table([accountId, accountEmail, accountCity,accountPassword,accountStatus]);
+//prefer not to use var datatype ,use let most of the time because var creates problem in functional scope and block scope
+
+
