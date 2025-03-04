386c2d533fa67f6bd4b0bafa0bd0a26e6f9d4542
Sakshipatade <sakshivpatade@gmail.com>
2025-03-04 12:44:11 +0530

    use of typeof

---
+++ 01_datatype.js
@@ -0,0 +1,12 @@
+"use strict";// it treats all js code as newer version of js 
+let name = "sakshi"
+let age = 20
+let city = "Pune"
+console.log(typeof "sakshi");
+console.log(typeof age);
+console.log(typeof city);
+
+
+//In this assignment we learn the use of "use strict" and use of "typeof"
+
+
