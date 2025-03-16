057cb51863021bb75033702455615ad7aaf20327
Sakshipatade <sakshivpatade@gmail.com>
2025-03-16 19:35:14 +0530

    use strict message

--- 01_datatype.js
+++ 01_datatype.js
@@ -1,4 +1,4 @@
-"use strict";// it treats all js code as newer version of js 
+"use strict";// it treats all js code as newer version of js  but there is no need to write it anyway js do it automaticaaly
 let name = "sakshi"
 let age = 20
 let city = "Pune"
@@ -8,5 +8,7 @@ console.log(typeof city);
 
 
 //In this assignment we learn the use of "use strict" and use of "typeof"
+
+
 
 
