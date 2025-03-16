4f01745cdfa86d21bff4b625aa9e69493bc22882
Sakshipatade <sakshivpatade@gmail.com>
2025-03-16 20:12:13 +0530

    Chanes in code

--- 02_comparison.js
+++ 02_comparison.js
@@ -1,19 +1,19 @@
 "use strict";
 //we are learning the comparison in js 
 
-// console.log(2>1);
-// console.log(2<1);
-// console.log(2<=1);
-// console.log(2>=1);
-// console.log(2==1);
+console.log(2>1);
+console.log(2<1);
+console.log(2<=1);
+console.log(2>=1);
+console.log(2==1);
 
 //this are the normal comparison as the datatype of both the value is "number" hence it becomes easy to give the op
 
 //----------------------------------------------------------------------------------------------------------------
 //now we are checking for the comparison between two diffrent datatypes 
 
-// console.log("2"<1);
-// console.log("2">1);
+console.log("2"<1);
+console.log("2">1);
 
 //here the first value is string and the second value is number , js converts the string into number by defalut and then compare those numbers
 //but it is necessary to compare the value which having same datatype only.
@@ -34,6 +34,11 @@ console.log(undefined >= 0);
 console.log(undefined <= 0);
 
 //here the output is always comes as false 
+
+//Note: When we use "==="(triple equal to) for comparison the engine of js strictly check the datatypes of the value and then give result as per it.
+
+
+console.log("2"===1);//output is false 
 
 
 
