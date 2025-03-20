d8eb0f8cf4b8dfd51b69f006c1406ca8982b21d0
Sakshipatade <sakshivpatade@gmail.com>
2025-03-20 19:25:10 +0530

    Example of js language

--- one.js
+++ one.js
@@ -4,7 +4,9 @@ let accountCity="pune"
 let accountPassword="76548"
 let accountStatus;
 
+console.log(accountId);
+
 console.table([accountId, accountEmail, accountCity,accountPassword,accountStatus]);
 //prefer not to use var datatype ,use let most of the time because var creates problem in functional scope and block scope
-
+//constant declared using const variable
 
