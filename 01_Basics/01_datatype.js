57f9dc6ed30cf6a03734408198aafcba7596e30e
Sakshipatade <sakshivpatade@gmail.com>
2025-03-21 13:40:42 +0530

    Moved to Main folder

---
+++ 01_Basics/01_datatype.js
@@ -0,0 +1,14 @@
+"use strict";// it treats all js code as newer version of js  but there is no need to write it anyway js do it automaticaaly
+let name = "sakshi"
+let age = 20
+let city = "Pune"
+console.log(typeof "sakshi");
+console.log(typeof age);
+console.log(typeof city);
+
+
+//In this assignment we learn the use of "use strict" and use of "typeof"
+
+
+
+
