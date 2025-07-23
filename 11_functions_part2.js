1511f0019830b926c30ec83e5c3c1a1a145d2f72
Sakshipatade <sakshivpatade@gmail.com>
2025-07-23 12:14:37 +0530

    Learn functions in deep

---
+++ 11_functions_part2.js
@@ -0,0 +1,18 @@
+function calculateMyCart(...num){
+    return num
+}
+console.log(calculateMyCart(200,400,100))
+//this is called as rest method or spread method
+
+
+let myObject = {
+    username:'sakshi',
+    price:300
+}
+
+function returnObjectValue(objValue){
+    return `Details are ${objValue.username} and ${objValue.price}}`
+    
+}
+console.log(returnObjectValue(myObject))
+
