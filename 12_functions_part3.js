245a75d8c8600544f118c0693f476eac1c44a034
Sakshipatade <sakshivpatade@gmail.com>
2025-08-06 22:02:29 +0530

    Funtions Part 3

---
+++ 12_functions_part3.js
@@ -0,0 +1,17 @@
+function calculateMyCart(...num){
+    return num
+}
+console.log(calculateMyCart(200,400,100)) 
+//this is called as rest method or spread method used most
+
+
+let myObject = {
+    username:'sakshi',
+    price:300
+}
+
+function returnObjectValue(objValue){
+    return `Details are ${objValue.username} and ${objValue.price}}`
+    
+}
+console.log(returnObjectValue(myObject))
