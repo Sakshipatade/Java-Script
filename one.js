ae79d36d3a47796b36b5aeea9674a9f2f43bf586
Sakshi Patade <121485256+Sakshipatade@users.noreply.github.com>
2025-08-23 09:46:24 +0530

    Update one.js

--- one.js
+++ one.js
@@ -1,11 +1,11 @@
-// // const accountId=67622
-// // let accountEmail="sakshi@gmail.com"
-// // let accountCity="pune"
-// // let accountPassword="76548"
-// // let accountStatus;
+const accountId=67622
+let accountEmail="sakshi@gmail.com"
+let accountCity="pune"
+let accountPassword="76548"
+let accountStatus;
 
-// // console.log(accountId);
-// console.log("10"+5);
+console.log(accountId);
+console.log("10"+5);
 
 // // console.table([accountId, accountEmail, accountCity,accountPassword,accountStatus]);
 // //prefer not to use var datatype ,use let most of the time because var creates problem in functional scope and block scope
