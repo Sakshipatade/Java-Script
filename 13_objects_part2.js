63649b38652276225ae96a49e12b098a379b4cd0
Sakshipatade <sakshivpatade@gmail.com>
2025-08-27 11:57:45 +0530

    Add nested object in part 2

--- 13_objects_part2.js
+++ 13_objects_part2.js
@@ -11,3 +11,17 @@ const car = {
 console.log(car.start()); // Toyota Fortuner is starting...
 
 
+//Nested Objects
+
+const student = {
+  name: "Sakshi",
+  grade: 9,
+  subjects: {
+    math: 85,
+    english: 90,
+    science: 88
+  }
+};
+
+console.log(student.subjects.math);   // 85
+console.log(student.subjects.english); // 90
