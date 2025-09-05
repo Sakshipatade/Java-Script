115fc5a38cb126ee22e93f7101ab208994d4a538
Sakshipatade <sakshivpatade@gmail.com>
2025-09-05 23:58:11 +0530

    Switch case example

---
+++ 19_switch_case.js
@@ -0,0 +1,24 @@
+const color = "red";
+
+switch (color) {
+    case "blue":
+        console.log("blue"); 
+        break;
+
+    case "green":
+        console.log("green"); 
+        break;
+
+    case "pink":
+        console.log("pink"); 
+        break;
+
+    case "red":
+        console.log("red executed"); 
+        break;
+
+    default:
+        console.log("black");
+        
+        break;
+}
