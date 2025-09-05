d24f8309161e7af8adcfba0ac18ea6482ea08e79
Sakshipatade <sakshivpatade@gmail.com>
2025-09-05 12:25:12 +0530

    another example add

--- 15_global_local_scope.js
+++ 15_global_local_scope.js
@@ -9,3 +9,21 @@ function addUser(){
 addUser()
 
 //console.log(userid);//usewr id will not be printed because it has local scope and we can not access outside that scope
+
+
+
+//----------------------------------------------
+
+//now let's check the scope accessibilty in the "if" loop
+
+if(username==="sakshi"){
+    const color = "red";
+
+    if(color==="red"){
+        const channel = "youtube"
+        console.log(color);
+        console.log(username);
+        
+    }
+
+}
