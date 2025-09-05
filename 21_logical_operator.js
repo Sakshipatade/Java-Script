035dbc39763bc0254f860b545791b083ae7738d7
Sakshipatade <sakshivpatade@gmail.com>
2025-09-06 00:12:52 +0530

    Use of logical operator

---
+++ 21_logical_operator.js
@@ -0,0 +1,19 @@
+const isUserLoggedIn = true;
+const haveDebitCard = true;
+const loggedInFromGoogle = true;
+const loggedInFromEmail = true;
+
+
+if (isUserLoggedIn && haveDebitCard) {
+    console.log("welcome you!!!");
+    
+}
+//use of && operator : both conditions should be true
+//we can also take value1 && value2 && value3
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("welcome you!!!");
+    
+}
+//either this or this : any one condtion should be true
+//we can also take value1 || value2 || value3
