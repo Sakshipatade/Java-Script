40258f696368c6d970f6501d2d91af8af89c28c4
Sakshipatade <sakshivpatade@gmail.com>
2025-09-06 00:17:32 +0530

    Use of Ternairy operator

---
+++ 22_null_descent.js
@@ -0,0 +1,23 @@
+let val1 = 10 ?? 15
+console.log(val1);
+
+
+let val2 = null ?? 13;
+console.log(val2);
+
+
+
+//ternairy operator and nullish method are two different things 
+
+
+
+//Ternairy operator
+
+//it is like if else conditon
+
+//Syntax : condition ? true : false
+
+
+let score = 100;
+score > 50 ? console.log("Score is greater than 50") : console.log("Score is not greater than 50");
+ 
