82507acf35510635bf1fb2ad34b9566922e6d10e
Sakshipatade <sakshivpatade@gmail.com>
2025-09-08 15:26:28 +0530

    Add Nested loop

--- 23_interations.js
+++ 23_interations.js
@@ -1,4 +1,18 @@
 for (let i = 0; i <= 10; i++ ) {
     const element = i;
-    console.log(element); 
+   // console.log(element); 
 }
+//This is the simple loop
+
+
+for (let i = 0; i <= 10 ; i++) {
+    console.log(`Outer Scope Value : ${i}`);
+
+        for (let j = 0; j <= 10; j++) {
+        
+            console.log(`Inner Scope values : ${j} and Outer Scope: ${i}`);
+            
+    }
+}
+
+//nested for loop
