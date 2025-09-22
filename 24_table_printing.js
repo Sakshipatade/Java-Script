79c8f0dd3d7f1b0f513991b007755ee788848710
Sakshipatade <sakshivpatade@gmail.com>
2025-09-22 23:05:51 +0530

    Use of break keyword

--- 24_table_printing.js
+++ 24_table_printing.js
@@ -5,3 +5,16 @@ for (let i = 1; i <= 10; i++) {
         console.log(i + "*" + j + "=" + i*j);
     }  
 }
+
+//if we want to break the control flow then we will use "break" keyword
+
+
+for (let i = 1; i <= 20; i++) {
+
+    if (i == 5) {
+        console.log("5 is detected");
+        break; //here we use break to break the flow of the control
+    }
+    console.log(`value of i is ${i}`) //this code will not printed because of the break keyword
+    
+}
