b0fc30998ced21ec52be1cfd16b9cd866095f2a5
Sakshipatade <sakshivpatade@gmail.com>
2025-09-26 18:05:49 +0530

    Pattern print

---
+++ 25_pattern_printing.js
@@ -0,0 +1,15 @@
+// for (let index = 0; index <= 10; index++) {
+//     let line = " ";
+
+//    for (let j = 0; j = j-index; j++) {
+//     console.log("*");
+//     line = line +1;
+    
+//    } 
+// }
+
+// let pattern_value = 0;
+// while (pattern_value <= 10) {
+//     console.log("*");
+//     pattern_value = pattern_value +1;  
+// }
