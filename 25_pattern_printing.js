c6bdde38e4a8a476725fa0de64ef9af3fa2c28f1
Sakshipatade <sakshivpatade@gmail.com>
2025-09-26 23:14:10 +0530

    restructure the code

--- 25_pattern_printing.js
+++ 25_pattern_printing.js
@@ -1,15 +1,18 @@
-// for (let index = 0; index <= 10; index++) {
-//     let line = " ";
-
-//    for (let j = 0; j = j-index; j++) {
-//     console.log("*");
-//     line = line +1;
+for(let i = 0; i <= 4; i++){
+    for(j = 0; j <= i; j++){
+        console.log("*");
+    }
+  //  console.log(" ");
     
-//    } 
-// }
+}
+5 
+
 
 // let pattern_value = 0;
 // while (pattern_value <= 10) {
 //     console.log("*");
 //     pattern_value = pattern_value +1;  
 // }
+
+
+
