bbd4cb75c7c53f14cff3a3753a4eb8de882378c2
Sakshipatade <sakshivpatade@gmail.com>
2025-09-26 23:48:47 +0530

    Print inverse table

--- 25_pattern_printing.js
+++ 25_pattern_printing.js
@@ -1,11 +1,23 @@
-for(let i = 0; i <= 4; i++){
-    for(j = 0; j <= i; j++){
-        process.stdout.write("*")
-    }
-    console.log(" ");  
-}
+// for(let i = 0; i <= 4; i++){
+//     for(j = 0; j <= i; j++){
+//         process.stdout.write("*")
+//     }
+//     console.log(" ");  
+// }
 //to prevent the output not printed on next line
 //js used process.stdout.write() method like above mentioned
+
+
+for(let i = 5; i >= 0; i--){
+  for(let j = 5; j >= i; j--)
+  {
+    process.stdout.write("*");
+
+  }
+  console.log(" ");
+}
+//this is also used to print the inverse table
+
 
 
 
