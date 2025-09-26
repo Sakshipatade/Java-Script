2befdb8e36df546dfdfe5a6ebc1f4ca5f16bed20
Sakshipatade <sakshivpatade@gmail.com>
2025-09-26 23:36:46 +0530

    pattern printed in inverse triangle

--- 25_pattern_printing.js
+++ 25_pattern_printing.js
@@ -1,18 +1,11 @@
 for(let i = 0; i <= 4; i++){
     for(j = 0; j <= i; j++){
-        console.log("*");
+        process.stdout.write("*")
     }
-  // console.log(" ");
-    
+    console.log(" ");  
 }
-//the output is not printing 
-
-
-// let pattern_value = 0;
-// while (pattern_value <= 10) {
-//     console.log("*");
-//     pattern_value = pattern_value +1;  
-// }
+//to prevent the output not printed on next line
+//js used process.stdout.write() method like above mentioned
 
 
 
