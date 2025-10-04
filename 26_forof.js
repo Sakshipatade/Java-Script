de368d8f6ea2a29dd8068a9d5e59928ead2ef561
Sakshipatade <sakshivpatade@gmail.com>
2025-10-04 21:15:59 +0530

    update 26 file

--- 26_forof.js
+++ 26_forof.js
@@ -1,15 +1,25 @@
-let arr = [1,2,3,4,5]
-for (const element of arr) {
-   console.log(element);
+// let arr = [1,2,3,4,5]
+// for (const element of arr) {
+//    console.log(element);
     
-}
+// }
 
-//now iterate in String
-let greetings = "Hello World!";
-for (const greet of greetings) {
-    if (greet == " ") {
-        break; // for breaking the execution of the code if any space occurs 
+// //now iterate in String
+// let greetings = "Hello World!";
+// for (const greet of greetings) {
+//     if (greet == " ") {
+//         break; // for breaking the execution of the code if any space occurs 
+//     }
+//     console.log(greet);
+// }
+
+let myName = "sakshi is my best friend";
+for (const element of myName) {
+    if (element == " ") {
+        console.log("-----");//dahs is printed where space detected
+        
     }
-    console.log(greet);
+    console.log(element);
+    
 }
 
