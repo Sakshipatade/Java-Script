6c223b7814f019c19ebb5e6b310dfb4b5f799f92
Sakshipatade <sakshivpatade@gmail.com>
2025-10-04 21:31:36 +0530

    Use of forin loop on array

--- 27_forin_loop.js
+++ 27_forin_loop.js
@@ -1,3 +1,5 @@
+
+//Use of forin loop on the Object
 let myObj = {
     'name': 'Sakshi',
     'city': 'Pune',
@@ -6,8 +8,19 @@ let myObj = {
 }
 
 for (const key in myObj) {
-    console.log(`My key is ${key} and its value is ${myObj[key]}`);  //here i wrote myObj[key] that is the value of object 
-    console.log(myObj[key]); //here only values will be printed
-    console.log(key); // here only key will be print like name, city etc
+   console.log(`My key is ${key} and its value is ${myObj[key]}`);  //here i wrote myObj[key] that is the value of object 
+   console.log(myObj[key]); //here only values will be printed
+   console.log(key); // here only key will be print like name, city etc
     
+}
+
+
+
+//Use of forin loop on arrays
+
+let myArr = ["Pune", "Mumbai", "Nashik", "Banglore", "Delhi"];
+
+for (const cityName in myArr) {
+    console.log(cityName); //it will print only the key values like 0 1 2 3 even if we dont give the key values like in the object example
+    console.log(myArr[cityName]); //so here the array values will get print
 }
