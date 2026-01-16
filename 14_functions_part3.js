2c3575135c9e23c3ccdbd805cb3c3962d7fd8b60
Sakshipatade <sakshivpatade@gmail.com>
2026-01-16 16:17:02 +0530

    restructure

--- 14_functions_part3.js
+++ 14_functions_part3.js
@@ -3,12 +3,16 @@ function greet() {
   console.log("Hello, World!");
 }
 greet(); // Calling the function
+//---------------------------------------------------------------------------------------
+
 
 //Function with Parameters and Return
 function add(a, b) {
   return a + b;
 }
 console.log(add(5, 3)); // 8 will be the output
+//--------------------------------------------------------------------------
+
 
 //Function Expression (Stored in a variable)
 const multiply = function(x, y) {
@@ -16,19 +20,28 @@ const multiply = function(x, y) {
 };
 console.log(multiply(4, 6)); // 24 will be the ouput
 
+
+//----------------------------------------------------------------------------------------
 //Arrow Function (ES6)
 const square = (num) => num * num;
 console.log(square(5)); // 25 will be the output
+
+//---------------------------------------------------------------------------------------
 
 //Anonymous Function (No name, used inside other functions)
 setTimeout(function() {
   console.log("This runs after 2 seconds");
 }, 2000);
 
+
+//---------------------------------------------------------------------------------------
 //Immediately Invoked Function Expression (IIFE)
 (function() {
   console.log("IIFE executed!");
 })();
+
+
+//---------------------------------------------------------------------------------------
 
 //Default Parameters in Functions
 function greetUser(name = "Guest") {
@@ -36,6 +49,10 @@ function greetUser(name = "Guest") {
 }
 greetUser();      // Hello, Guest <- This is the output
 greetUser("Sakshi"); // Hello, Sakshi <- This is the output
+
+
+
+//---------------------------------------------------------------------------------------
 
 //Rest Parameters (...args)
 //To accept multiple values as an array.
