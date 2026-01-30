006407b97e80d730a77b49809640a8cee6230184
Sakshipatade <sakshivpatade@gmail.com>
2026-01-30 12:52:16 +0530

    example of reduce method

--- 29_filter.js
+++ 29_filter.js
@@ -1,61 +1,79 @@
-const myArray = [1,2,3,4,5,6,7,8,9,10];
+// const myArray = [1,2,3,4,5,6,7,8,9,10];
 
-const newArray = myArray.filter( (num) => {
-    return num > 6
-} )
+// const newArray = myArray.filter( (num) => {
+//     return num > 6
+// } )
 
-//console.log(newArray);
-//we can print array using scope but need to always return value 
-
-
+// //console.log(newArray);
+// //we can print array using scope but need to always return value 
 
 
-const myArray1 = [1,2,3,4,5,6,7];
-
-const newArray1 = myArray1.filter( (num) => num > 4  )
-//console.log(newArray1);
-//we can also print array in one line only like the above example
 
 
-//filter method is game of only true and false 
+// const myArray1 = [1,2,3,4,5,6,7];
+
+// const newArray1 = myArray1.filter( (num) => num > 4  )
+// //console.log(newArray1);
+// //we can also print array in one line only like the above example
 
 
-const books = [
-    {
-        name : 'book 1', genre : 'history', publish : 1990,  publish : 2000
-    },
-     {
-        name : 'book 2', genre : 'sports', publish : 2000,  publish : 2003
-    },
-     {
-        name : 'book 3', genre : 'history', publish : 1999,  publish : 2025
-    },
-     {
-        name : 'book 4', genre : 'technicaal', publish : 2003,  publish : 2013
-    },
-     {
-        name : 'book 5', genre : 'arts', publish : 2005,  publish : 2010
-    },
-     {
-        name : 'book 6', genre : 'history', publish : 1980,  publish : 1996
-    },
-     {
-        name : 'book 7', genre : 'sports', publish : 2000,  publish : 2004
-    },
-     {
-        name : 'book 8', genre : 'technical', publish : 1980,  publish : 2022
-    },
-     {
-        name : 'book 9', genre : 'arts', publish : 2014,  publish : 2025
-    },
-     {
-        name : 'book 10', genre : 'technical', publish : 2020,  publish : 2025
-    }
-]
+// //filter method is game of only true and false 
 
-const book_details = books.filter( (bk) => {
-    return bk.genre === 'history' && bk.publish >= 2000
-} )
 
-console.log(book_details);
-//This is how we can apply condition to specifically fetch the details 
+// const books = [
+//     {
+//         name : 'book 1', genre : 'history', publish : 1990,  publish : 2000
+//     },
+//      {
+//         name : 'book 2', genre : 'sports', publish : 2000,  publish : 2003
+//     },
+//      {
+//         name : 'book 3', genre : 'history', publish : 1999,  publish : 2025
+//     },
+//      {
+//         name : 'book 4', genre : 'technicaal', publish : 2003,  publish : 2013
+//     },
+//      {
+//         name : 'book 5', genre : 'arts', publish : 2005,  publish : 2010
+//     },
+//      {
+//         name : 'book 6', genre : 'history', publish : 1980,  publish : 1996
+//     },
+//      {
+//         name : 'book 7', genre : 'sports', publish : 2000,  publish : 2004
+//     },
+//      {
+//         name : 'book 8', genre : 'technical', publish : 1980,  publish : 2022
+//     },
+//      {
+//         name : 'book 9', genre : 'arts', publish : 2014,  publish : 2025
+//     },
+//      {
+//         name : 'book 10', genre : 'technical', publish : 2020,  publish : 2025
+//     }
+// ]
+
+// const book_details = books.filter( (bk) => {
+//     return bk.genre === 'history' && bk.publish >= 2000
+// } )
+
+// console.log(book_details);
+// //This is how we can apply condition to specifically fetch the details 
+
+
+
+const arr = [5, 1, 3, 2, 6];
+
+// function filterArr(num){
+//     return num > 2
+// }
+// console.log(arr.filter(filterArr))
+
+
+console.log(arr.filter(function filterArr(num){
+    return num > 2
+}));
+
+console.log(arr.filter( (num) => {
+    return num > 2
+}));
